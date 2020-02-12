8ca58cb78df8961810889a46907e0063b5cfdf99
David Mihal <david@dmihal.com>
2020-02-12 09:11:41 -0700

    Create truffle project

---
+++ contracts/contracts/NameTag.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+import './FreeGas.sol';
+
+contract NameTag is FreeGas {
+  mapping(address => string) private _names;
+
+  event NameSet(address indexed user, string name);
+
+  function getName(address user) external view returns (string memory) {
+    return _names[user];
+  }
+
+  function setName(string calldata newName) external {
+    _names[msg.sender] = newName;
+    emit NameSet(msg.sender, newName);
+  }
+}
